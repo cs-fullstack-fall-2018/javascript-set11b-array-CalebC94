fdccbeed5bd5fd5a936932d8adc562761238cb2f
Caleb Canada <calebcanada13@gmail.com>
2018-08-01 14:50:59 -0500

    Check in

---
+++ Ex28.js
@@ -0,0 +1,29 @@
+var myArray = [];
+
+var arrayItem = myArray.push("Caleb", "JP","PJ","Kevin");
+
+document.write(myArray);
+console.log(myArray);
+
+
+
+
+
+var myArray1 = [10, 22, 94, 13];
+
+document.write(myArray1)
+console.log(myArray1)
+
+
+
+
+var myArray2 = ["SubZero", "Scorpion", "LuiKang", "Ermac"];
+
+console.log(myArray2[2]);
+
+var pos =1;
+
+var removedItem = myArray2.splice(pos,1);
+
+console.log(myArray2[2]);
+
